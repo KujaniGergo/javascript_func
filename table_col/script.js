ff67cd530b2477aa3a4a2053b3d642c7c85a5d66
Kujani Gergo <kujani.gergo@BOLYAI.LOCAL>
2025-10-03 12:23:01 +0200

    javascript alapok

---
+++ table_col/script.js
@@ -0,0 +1,29 @@
+console.log("alma")
+
+const alma = "alma"
+
+console.log(alma)
+
+const korte = ["sd","df","sd"]
+
+console.log(korte)
+
+ for (let i = 0; i < korte.length; i++) 
+ {
+    console.log = korte[i];
+ }
+
+/**
+ * @type {{name:string,age:int}}
+ */
+
+ const y = 
+ {
+    name :"gombszab",
+    age : 25,
+ }
+
+ for(const key in y)
+ {
+    y[key]
+ }
