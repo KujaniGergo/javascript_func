681b95430a007ba649332d92e332d2af3c663877
kujani gergo <kujani.gergo_2022c@bolyai-szakkozep.hu>
2025-10-08 06:36:25 -0700

    hazi

--- table_col/script.js
+++ table_col/script.js
@@ -1,29 +1,123 @@
-console.log("alma")
-
-const alma = "alma"
-
-console.log(alma)
-
-const korte = ["sd","df","sd"]
-
-console.log(korte)
-
- for (let i = 0; i < korte.length; i++) 
- {
-    console.log = korte[i];
- }
-
 /**
- * @type {{name:string,age:int}}
+ * @type {{neve: string, kora:string, szeretője1:string, szeretője2?:string}[]}
  */
 
- const y = 
- {
-    name :"gombszab",
-    age : 25,
- }
 
- for(const key in y)
- {
-    y[key]
- }
+const bumm = "1, 2, 3, 4, 5"
+console.log(bumm)
+const b=["a","b","c"]
+for(let i=0;i<b.length;i++)
+{
+    console.log(b[i])
+}
+console.log(b[1])
+
+
+for(const a of b)
+{
+    console.log(a)
+}
+
+
+for(const key in b)
+{
+    console.log(`${key}: ${b[key]}`)
+}
+
+
+const objektum=
+{
+    name : "Gergő",
+    age : 17
+}
+
+
+for(const key in objektum)
+{
+    console.log(objektum[key])
+}
+
+
+const arr = [
+    {
+        kora: 'reformáció',
+        neve: 'Balassi Bálint',
+        szeretője1: 'Losonczy Anna',
+        szeretője2: 'Dobó Krisztina'
+    },
+    {
+        kora: 'felvilágosodás',
+        neve: 'Csokonai',
+        szeretője1: 'Vajda Juliána',
+        
+    },
+    {
+        kora: 'magyar romantika',
+        neve: 'Petőfi Sándor',
+        szeretője1: 'Mednyánszky Berta',
+        szeretője2: 'Szendrey Júlia'
+    },
+    {
+        kora: '20. század',
+        neve: 'Ady Endre',
+        szeretője1: 'Léda',
+        szeretője2: 'Csinszka'
+    }
+]
+
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tr = document.createElement("tr");
+const th1 = document.createElement("th");
+const th2 = document.createElement("th");
+const th3 = document.createElement("th");
+const tbody = document.createElement("tbody");
+
+document.body.appendChild(table)
+table.appendChild(thead)
+tr.appendChild(thead)
+th1.appendChild(tr)
+th2.appendChild(tr)
+th3.appendChild(tr)
+table.appendChild(tbody)
+
+th1.innerText = "Szerző neve"
+th2.innerText = "IdőKorszakszak"
+th3.innerText = "Szeretői"
+th3.colSpan = 2
+
+
+for (const szamlalo of arr) 
+{
+    const tr = document.createElement('tr')
+    tbody.appendChild(tr)
+
+
+    const td = document.createElement('td')
+    td.innerText = szamlalo.neve
+    tr.appendChild(td)
+
+
+    const td2 = document.createElement('td')
+    td2.innerText = szamlalo.kora
+    tr.appendChild(td2)
+
+
+    const td3= document.createElement('td')
+    td3.innerText = szamlalo.szeretője1
+    tr.appendChild(td3)
+
+
+    if (szamlalo.szeretője2 === undefined)
+    {
+         td3.colSpan = 2
+    }
+
+
+    else
+    {
+    const td4 = document.createElement('td')
+    td4.innerText = szamlalo.szeretője2
+    tr.appendChild(td4)
+    }
+}
--- table_col_hf/script.js
+++ table_col_hf/script.js
@@ -1,11 +1,11 @@
 /**
- * @type {{theme:string,time:string,scientist1:string,scientist2:string}[]}
+ * @type {{theme: string, time:string, scientist1:string, scientist2?:string}[]}
  */
 
 const arr = [
     {
         theme: 'Optika',
-        time: 'XI. szazad',
+        time: 'Xszamlalo. szazad',
         scientist1: 'Alhazen'
     },
     {
@@ -22,7 +22,7 @@ const arr = [
     },
     {
         theme: 'Modern fizika',
-        time: 'XX-XXI. század',
+        time: 'XX-XXszamlalo. század',
         scientist1: 'Richard Feynman',
         scientist2: 'Stephen Hawking'
     }
@@ -36,14 +36,53 @@ const th2 = document.createElement("th");
 const th3 = document.createElement("th");
 const tbody = document.createElement("tbody");
 
-table.appendChild(table)
-thead.appendChild(thead)
-tr.appendChild(tr)
-th1.appendChild(th1)
-th2.appendChild(th2)
-th3.appendChild(th3)
+document.body.appendChild(table)
+table.appendChild(thead)
+tr.appendChild(thead)
+th1.appendChild(tr)
+th2.appendChild(tr)
+th3.appendChild(tr)
 table.appendChild(tbody)
 
 th1.innerText = "Fizika területe"
 th2.innerText = "Időszak"
 th3.innerText = "Képviselők"
+
+
+th3.colSpan = 2
+
+
+for (const szamlalo of arr) 
+{
+    const tr = document.createElement('tr')
+    tbody.appendChild(tr)
+
+
+    const td = document.createElement('td')
+    td.innerText = szamlalo.theme
+    tr.appendChild(td)
+
+
+    const td2 = document.createElement('td')
+    td2.innerText = szamlalo.time
+    tr.appendChild(td2)
+
+
+    const td3= document.createElement('td')
+    td3.innerText = szamlalo.scientist1
+    tr.appendChild(td3)
+
+
+    if (szamlalo.scientist2 === undefined)
+    {
+         td3.colSpan = 2
+    }
+
+
+    else
+    {
+    const td4 = document.createElement('td')
+    td4.innerText = szamlalo.scientist2
+    tr.appendChild(td4)
+    }
+}
