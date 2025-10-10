b6849b7065975f3a7f8c9e96b1aafe66b11d8c96
Kujani Gergo <kujani.gergo@BOLYAI.LOCAL>
2025-10-10 11:42:39 +0200

    orai

--- table_row/kitero.js
+++ table_row/kitero.js
@@ -1,21 +1,20 @@
-const a = {}
+const valt = {}
 const szam = 12
 
 
 if (szam > 5) 
 {
-    a.name("nev")
+    valt.name = "nev"
 }else
 {
-    a["name"]="nev"
+    valt["name"]="nev"
 }
 
-a.name = "name"
-console.log(a.name)
-console.log(a["name"])
+console.log(valt.name)
+console.log(valt["name"])
 
-//a["name"]=String ugyan az
-//a.a = "Gergő" ugyan az
+//valt["name"]=String ugyan az
+//valt = "Gergő" ugyan az
 
 //5==="5" True
 //5=="5"  False
